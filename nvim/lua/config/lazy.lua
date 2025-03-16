9363832dbfd070b58d973c161366422037d8bf2d
see-quick <maros.orsak159@gmail.com>
2025-03-16 19:58:00 +0100

    Update nvim quint LSP

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,12 +13,13 @@ end
 vim.opt.termguicolors = true
 vim.opt.rtp:prepend(lazypath)
 -- this is basically for quint formal specification language because Mason theer is no such as `quint`
-vim.cmd [[
-    au BufNewFile,BufReadPost *.qnt runtime syntax/quint.vim
-]]
+--vim.cmd [[
+--    au BufNewFile,BufReadPost *.qnt runtime syntax/quint.vim
+--]]
 vim.cmd [[
   au BufRead,BufNewFile *.qnt setfiletype quint
 ]]
+
 vim.g.coq_settings = {
   auto_start = 'shut-up', -- Automatically starts COQ in the background
   clients = {
@@ -27,7 +28,6 @@ vim.g.coq_settings = {
   },
 }
 vim.lsp.set_log_level("debug")
-
 
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
   checker = {
